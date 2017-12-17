10bc48ea37abfb48d527c33a4c2d1ab0036570ca
Michael Aquilina <michaelaquilina@gmail.com>
2017-12-17 15:36:04 +0100

    Day 1 - Part 1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,41 @@
+extern crate clap;
+use clap::{Arg, App};
+
+use std::fs::File;
+use std::io::prelude::*;
+
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("Advent of Code 2017 - Day 1")
+        .arg(Arg::with_name("filename")
+             .required(true))
+        .get_matches();
+
+    let filename = matches.value_of("filename").unwrap();
+
+    let mut file = File::open(filename).
+        expect("File not found");
+
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)
+        .expect("Something went wrong when reading the file");
+    contents = contents.trim().to_string();
+
+    let length = contents.len();
+
+    let mut total: u32 = 0;
+    let mut index: usize = 0;
+    while index < contents.len() {
+        let next = (index + 1) % length;
+
+        let v1 = contents.chars().nth(index).unwrap();
+        let v2 = contents.chars().nth(next).unwrap();
+
+        if v1 == v2 {
+            total += v1.to_digit(10).expect("Not a digit!");
+        }
+
+        index += 1;
+    }
+    println!("{}", total);
 }
